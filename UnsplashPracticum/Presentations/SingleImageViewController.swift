e3877a72db851009d5124917912aca666086252d
Makhlenko Sergey <mahlenko-weblive@yandex.ru>
2023-01-07 22:00:25 +0300

    resize and centered image

--- UnsplashPracticum/Presentations/SingleImageViewController.swift
+++ UnsplashPracticum/Presentations/SingleImageViewController.swift
@@ -27,79 +27,52 @@ class SingleImageViewController: UIViewController {
 
         scrollView.minimumZoomScale = 0.1
         scrollView.maximumZoomScale = maxScale
+
+        // размеры и позиционирование изображения
+        scrollView.setZoomScale(getScaleImage(), animated: false)
+        centeredImage()
     }
 
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        imageFillScreen(image: image)
-        imageCentered()
+    private func centeredImage() {
+        print("↔️↕️Centering the image on the screen...")
+
+        scrollView.layoutIfNeeded()
+
+        let viewSize = scrollView.bounds.size
+        let contentSize = scrollView.contentSize
+
+        let offset = (
+            left: Double((contentSize.width - viewSize.width) / 2),
+            top: Double((contentSize.height - viewSize.height) / 2))
+
+        scrollView.setContentOffset(
+                CGPoint(
+                    x: offset.left,
+                    y: offset.top),
+                animated: false)
+
+        print("Offsets: \(offset)")
     }
 
-    func imageFillScreen(image: UIImage) {
-        let visibleRectSize = scrollView.bounds.size
-        let imageSize = imageView.bounds.size
+    /// Вернет масштаб изображения
+    private func getScaleImage() -> CGFloat {
+        let minScale = scrollView.minimumZoomScale
+        let maxScale = scrollView.maximumZoomScale
 
-        let scale = min(
-            maxScale,
-            max(
-                scrollView.minimumZoomScale,
-                max(visibleRectSize.width / imageSize.width, visibleRectSize.height / imageSize.height)
-            ))
+        let imageSize = image.size
+        let viewSize = scrollView.bounds.size
 
-        view.layoutIfNeeded()
-        scrollView.setZoomScale(scale, animated: true)
+        let wScale = viewSize.height / imageSize.height
+        let hScale = viewSize.width / imageSize.width
+
+        let theoreticalScale = max(hScale, wScale)
+
+        return min(maxScale, max(minScale, theoreticalScale))
     }
-
-    func imageCentered() {
-        let scrollSize = scrollView.bounds.size
-        var imageFrame = imageView.frame ?? CGRect.zero
-
-        if imageFrame.size.width < scrollSize.width {
-            imageFrame.origin.x = (scrollSize.width - imageFrame.size.width) * 0.5
-        } else {
-            imageFrame.origin.x = 0.0
-        }
-
-        if imageFrame.size.height < scrollSize.height {
-            imageFrame.origin.y = (scrollSize.height - imageFrame.size.height) * 0.5
-        } else {
-            imageFrame.origin.y = 0.0
-        }
-
-        imageView.frame = imageFrame
-    }
-
-//    private func rescaleAndCenterImageInScrollView(image: UIImage) {
-//        let minZoomScale = scrollView.minimumZoomScale
-//        let maxZoomScale = scrollView.maximumZoomScale
-//
-//        view.layoutIfNeeded()
-//        let visibleRectSize = scrollView.bounds.size
-//        let imageSize = image.size
-//        let hScale = visibleRectSize.width / imageSize.width
-//        let vScale = visibleRectSize.height / imageSize.height
-//        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
-//        scrollView.setZoomScale(scale, animated: false)
-//
-//        scrollView.layoutIfNeeded()
-//        let newContentSize = scrollView.contentSize
-//        let x = (newContentSize.width - visibleRectSize.width) / 2
-//        let y = (newContentSize.height - visibleRectSize.height) / 2
-//        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
-//    }
 }
 
 extension SingleImageViewController: UIScrollViewDelegate {
-//    open override func viewDidLayoutSubviews() {
-//        super.viewDidLayoutSubviews()
-//        imageCentered()
-//    }
-
     public func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         imageView
-    }
-
-    public func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
-        imageCentered()
     }
 }
