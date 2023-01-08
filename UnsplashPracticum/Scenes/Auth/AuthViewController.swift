75604b3c936bfa21ad0b8ecde975c763fc7dd0e5
Makhlenko Sergey <mahlenko-weblive@yandex.ru>
2023-01-08 18:01:10 +0300

    Add login scene

---
+++ UnsplashPracticum/Scenes/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+// Created by Сергей Махленко on 08.01.2023.
+// Copyright (c) 2023 ru.app-demo.unsplash. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
+}
