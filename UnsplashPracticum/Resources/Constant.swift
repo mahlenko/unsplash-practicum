d5c5257030253fdf3ef87452f7029fea3c80d55d
Makhlenko Sergey <mahlenko-weblive@yandex.ru>
2023-01-08 18:01:14 +0300

    Add login scene

---
+++ UnsplashPracticum/Resources/Constant.swift
@@ -0,0 +1,18 @@
+//
+// Created by Сергей Махленко on 08.01.2023.
+// Copyright (c) 2023 ru.app-demo.unsplash. All rights reserved.
+//
+
+import Foundation
+
+enum Constant: String {
+    //
+    case unsplashOauthURL = "https://unsplash.com/oauth"
+    case unsplashBaseURL = "https://api.unsplash.com"
+
+    //
+    case accessKey = "nOLcOHUH7Ts4Cmd04SfIikzffu3i7g9OqAiReZJSAoA"
+    case secretKey = "XM2UQisS3ZUPEJQLxAgwKtXpxevIrqVonKadTLcZKj8"
+    case redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    case accessScope = "public+read_user+write_likes"
+}
